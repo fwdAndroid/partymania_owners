9f39d46a6362251839138162441eee3dd2b103a4
fawad kaleem <fwdkaleem@gmail.com>
2023-08-24 20:35:21 +0500

    Model Signup

---
+++ lib/model/signup_models.dart
@@ -0,0 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class SignUp_Model {
+  String uid;
+  String email;
+  String fullName;
+  String phone_Number;
+  String createPassword;
+  String confrimPassword;
+
+  SignUp_Model(
+      {required this.uid,
+      required this.email,
+      required this.fullName,
+      required this.createPassword,
+      required this.confrimPassword,
+      required this.phone_Number});
+
+  ///Converting OBject into Json Object
+  Map<String, dynamic> toJson() => {
+        'phone_Number': phone_Number,
+        'uid': uid,
+        'email': email,
+        'fullName': fullName,
+        'confrimPassword': confrimPassword,
+        'createPassword': createPassword
+      };
+
+  ///
+  static SignUp_Model fromSnap(DocumentSnapshot snaps) {
+    var snapshot = snaps.data() as Map<String, dynamic>;
+
+    return SignUp_Model(
+      phone_Number: snapshot['phone_Number'],
+      uid: snapshot['uid'],
+      email: snapshot['email'],
+      createPassword: snapshot['createPassword'],
+      fullName: snapshot['fullName'],
+      confrimPassword: snapshot['confrimPassword'],
+    );
+  }
+}
