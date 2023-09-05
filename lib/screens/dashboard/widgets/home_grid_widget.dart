82ab2711acd742ed9fb727049cb01bd005832e19
fawad kaleem <fwdkaleem@gmail.com>
2023-09-05 15:42:53 +0500

    Grid

--- lib/screens/dashboard/widgets/home_grid_widget.dart
+++ lib/screens/dashboard/widgets/home_grid_widget.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:partymania_owners/utils/colors.dart';
 
@@ -11,20 +13,165 @@ class HomeGridWidget extends StatefulWidget {
 class _HomeGridWidgetState extends State<HomeGridWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 250,
-      child: GridView.builder(
-        scrollDirection: Axis.horizontal,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 1, // Set the number of items per row
-        ),
-        itemBuilder: (BuildContext context, int index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset("assets/Event listing.png"),
+    return StreamBuilder<Object>(
+        stream: FirebaseFirestore.instance
+            .collection("events")
+            .where("uid", isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return Container(
+            height: 290,
+            child: GridView.builder(
+              scrollDirection: Axis.horizontal,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 1, // Set the number of items per row
+              ),
+              itemBuilder: (BuildContext context, int index) {
+                final List<DocumentSnapshot> documents = snapshot.data!.docs;
+
+                final Map<String, dynamic> data =
+                    documents[index].data() as Map<String, dynamic>;
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        border: Border.all(color: textColor)),
+                    height: 300,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: 150,
+                          child: Stack(
+                            children: [
+                              ClipRRect(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)),
+                                child: Image.network(
+                                  data['eventCoverPhoto'],
+                                  fit: BoxFit.cover,
+                                  height: 150,
+                                  width: MediaQuery.of(context).size.width,
+                                ),
+                              ),
+                              Align(
+                                alignment: AlignmentDirectional.bottomCenter,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Container(
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(16),
+                                            border: Border.all(
+                                                width: 1,
+                                                color: textColor
+                                                    .withOpacity(.80))),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Text(
+                                            data['eventDate'],
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          ),
+                                        ),
+                                      ),
+                                      Container(
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(16),
+                                            border: Border.all(
+                                                width: 1,
+                                                color: textColor
+                                                    .withOpacity(.80))),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Text(
+                                            "4.9",
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            data['eventName'],
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 14),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              Container(
+                                  decoration: BoxDecoration(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(16)),
+                                      border: Border.all(color: otpColor)),
+                                  height: 30,
+                                  width: 50,
+                                  child: Center(
+                                    child: Text(
+                                      "Music",
+                                      style: TextStyle(color: otpColor),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  )),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Image.asset(
+                                "assets/Auto Layout Horizontal.png",
+                              )
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.location_pin,
+                                color: otpColor,
+                                size: 15,
+                              ),
+                              Text(
+                                data['eventLocation'],
+                                style: TextStyle(
+                                    color: otpColor,
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 12),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
           );
-        },
-      ),
-    );
+        });
   }
 }
