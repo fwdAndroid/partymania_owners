f76b3b825d2cf9a484f671eeafb1df720dfa8be8
fawad kaleem <fwdkaleem@gmail.com>
2023-09-15 07:29:35 +0500

    Otp Implemented

--- lib/screens/auth/getotp.dart
+++ lib/screens/auth/getotp.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:partymania_owners/screens/auth/login_screen.dart';
+import 'package:partymania_owners/screens/auth/otp/verify_phone.dart';
 import 'package:partymania_owners/utils/button.dart';
 import 'package:partymania_owners/utils/colors.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+
+import 'package:country_code_picker/country_code_picker.dart';
 
 class GetOtp extends StatefulWidget {
   const GetOtp({super.key});
@@ -11,6 +15,9 @@ class GetOtp extends StatefulWidget {
 }
 
 class _GetOtpState extends State<GetOtp> {
+  TextEditingController _controller = TextEditingController();
+
+  String dialCodeDigits = "+91";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,14 +30,14 @@ class _GetOtpState extends State<GetOtp> {
             const SizedBox(
               height: 60,
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "00:42",
-                style: TextStyle(
-                    fontSize: 24,
-                    color: textColor,
-                    fontWeight: FontWeight.w600),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset(
+                  "assets/logo.png",
+                  width: 150,
+                  height: 200,
+                ),
               ),
             ),
             Padding(
@@ -43,11 +50,39 @@ class _GetOtpState extends State<GetOtp> {
                     fontWeight: FontWeight.w600),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset(
-                "assets/otp.png",
-                height: 100,
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blueAccent),
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white70),
+              margin: EdgeInsets.only(left: 20, right: 20, top: 10),
+              child: Row(
+                children: [
+                  CountryCodePicker(
+                      onChanged: (country) {
+                        setState(() {
+                          dialCodeDigits = country.dialCode!;
+                        });
+                      },
+                      initialSelection: "IN",
+                      showCountryOnly: false,
+                      showOnlyCountryWhenClosed: false,
+                      favorite: ["+91", "IND"]),
+                  Expanded(
+                    child: Container(
+                      child: TextFormField(
+                        validator: RequiredValidator(errorText: "Required"),
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "7665060613",
+                          //  prefix: Padding(padding: EdgeInsets.all(10),child: Text(dialCodeDigits,style: TextStyle(color: Colors.black),),),
+                        ),
+                        keyboardType: TextInputType.number,
+                        controller: _controller,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
             Padding(
@@ -59,7 +94,9 @@ class _GetOtpState extends State<GetOtp> {
                       Navigator.push(
                           context,
                           MaterialPageRoute(
-                              builder: (builder) => LoginScreen()));
+                              builder: (builder) => VerifyPhone(
+                                  codeDigits: dialCodeDigits,
+                                  phone: _controller.text)));
                     }),
               ),
             )
