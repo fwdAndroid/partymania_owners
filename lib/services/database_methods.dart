846d7b2ff3c67cc078e50a53332a7fdb7f1f2dc1
fawad kaleem <fwdkaleem@gmail.com>
2023-10-20 21:11:49 +0500

    Nearly Done

--- lib/services/database_methods.dart
+++ lib/services/database_methods.dart
@@ -59,55 +59,6 @@ class FirebaseMethods {
     return res;
   }
 
-  Future<String> updateClub(
-      Uint8List coverPhoto,
-      Uint8List ticketPhoto,
-      String description,
-      String uid,
-      String clubLocation,
-      String clubPhoneNumber,
-      String clubName,
-      String clubType,
-      String clubState,
-      String clubCity,
-      String clubCountry,
-      String clubZipCode,
-      List<String> clubAmentities) async {
-    String res = "Some Information Regarding Club is Missing";
-    try {
-      String clubPhoto = await StorageMethods()
-          .uploadImageToStorage("clubCoverPhoto", coverPhoto, true);
-      String clubTicketPhoto = await StorageMethods()
-          .uploadImageToStorage("clubTablePhoto", ticketPhoto, true);
-      ClubModel postModel = ClubModel(
-          followers: [],
-          coverPhoto: clubPhoto,
-          clubState: clubState,
-          clubType: clubType,
-          ticketPhoto: clubTicketPhoto,
-          clubZipCode: clubZipCode,
-          uid: FirebaseAuth.instance.currentUser!.uid,
-          clubCity: clubCity,
-          clubAmentities: clubAmentities,
-          clubDescription: description,
-          clubLocation: clubLocation,
-          clubName: clubName,
-          clubCountry: clubCountry,
-          clubPhoneNumber: clubPhoneNumber);
-
-      ///Uploading Post To Firebase
-      _firebaseFirestore
-          .collection('clubs')
-          .doc(FirebaseAuth.instance.currentUser!.uid)
-          .update(postModel.toJson());
-      res = 'Successfully Club is Updated';
-    } catch (e) {
-      res = e.toString();
-    }
-
-    return res;
-  }
-
   //Event
   Future<String> createEvent(
       String eventName,
